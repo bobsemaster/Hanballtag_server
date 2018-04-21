bbb23646dd935136a60add8c02dc0c6543d54b64
Robert.Schreib <robert.schreib@e-mundo.de>
2018-04-21 21:21:09 +0200

    Initial Commit without ci

---
+++ src/main/kotlin/de/schreib/handball/handballtag/HandballtagApplication.kt
@@ -0,0 +1,11 @@
+package de.schreib.handball.handballtag
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HandballtagApplication
+
+fun main(args: Array<String>) {
+    runApplication<HandballtagApplication>(*args)
+}
---
+++ src/test/kotlin/de/schreib/handball/handballtag/HandballtagApplicationTests.kt
@@ -0,0 +1,18 @@
+package de.schreib.handball.handballtag
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+@ActiveProfiles("test")
+class HandballtagApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
