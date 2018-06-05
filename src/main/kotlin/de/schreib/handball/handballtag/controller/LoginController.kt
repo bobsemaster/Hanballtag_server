d5b0d648074d5c033d555b2042882ef67d98a5ae
Robert.Schreib <robert.schreib@e-mundo.de>
2018-06-05 12:25:35 +0200

    Logging für login und null handling

--- src/main/kotlin/de/schreib/handball/handballtag/controller/LoginController.kt
+++ src/main/kotlin/de/schreib/handball/handballtag/controller/LoginController.kt
@@ -1,5 +1,6 @@
 package de.schreib.handball.handballtag.controller
 
+import org.slf4j.LoggerFactory
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.CrossOrigin
@@ -11,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController
 @CrossOrigin()
 @RequestMapping("/login")
 class LoginController {
+    private val log = LoggerFactory.getLogger(this::class.java)
     @GetMapping
-    fun getAuthenticatedUser(@AuthenticationPrincipal user:UserDetails): UserDetails {
+    fun getAuthenticatedUser(@AuthenticationPrincipal user:UserDetails?): UserDetails? {
+        if(user == null) {
+            log.error("User nicht angemeldet!")
+        }
         return user
     }
 }
